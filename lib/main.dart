bbdaf904cfc7a7faa9df73c7c57e7231f6c64339
MrJackedUpBear <79388021+MrJackedUpBear@users.noreply.github.com>
2025-10-31 14:59:39 -0500

    Improve comments for better code readability
    Refactor comments for clarity and conciseness throughout the code.

--- lib/main.dart
+++ lib/main.dart
@@ -7,59 +7,44 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // root of the application
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //Sets the title and themdata of the page
       title: 'Book DB',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF7F5539)),
       ),
+      //sets the home page by calling MyHomePage widget with the title.
       home: const MyHomePage(title: 'Book DB'),
     );
   }
 }
 
+//MyHomePage widget that displays my home page.
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  //sets the title.
   final String title;
 
+  //Creates a homepage state.
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+//Review class to hold different review information for a book.
 class Review{
+  //intitialized the title, description, and rating for the review
   var title = "";
   var description = "";
   var rating = 0.0;
 
+  //constructor class to initialize some values for the review.
   Review(this.title, this.description, this.rating);
 
+  //Normal setters and getters for the title, description, and rating
   void setTitle(String title){this.title = title;}
 
   void setDescription(String description){this.description = description;}
@@ -71,7 +56,9 @@ class Review{
   double getRating(){return rating;}
 }
 
+//Book class to hold all necessary information about a given book
 class Book{
+  //initializes the book title, genre, author, reviews, description, availability, isbn, and owner.
   var _title = "";
   var _genre = "";
   var _author = "";
@@ -81,16 +68,19 @@ class Book{
   var _isbn = "";
   var _owner = "";
 
+  //Constructor class for Book
   Book(this._title, this._genre, this._author,
       this._description, this._availability,
       this._isbn, this._owner);
 
+  //Normal setters to set the title, genre, author, description, availability, isbn, and owner.
   void setTitle(String title){_title = title;}
 
   void setGenre(String genre){_genre = genre;}
 
   void setAuthor(String author){_author = author;}
 
+  //This is a setter to add a review to the list of reviews
   void addReview(Review review) {_reviews.add(review);}
 
   void setDescription(String description){_description = description;}
@@ -101,34 +91,45 @@ class Book{
 
   void setOwner(String owner){_owner = owner;}
 
+  //Normal getters to get the title, genre, author, description, availability, isbn, and owner.
   String getTitle(){return _title;}
 
   String getGenre(){return _genre;}
 
   String getAuthor(){return _author;}
 
+  //This gets a specific review given an index.
   Review getReview(int index){
+    //Checks if the index is a valid index. If not, returns an empty review.
     if (index < 0 || index > _reviews.length){
       return Review("", "", 0);
     }
 
+    //Returns the element at the index given.
     return _reviews.elementAt(index);
   }
 
+  //Returns the list of reviews
   List<Review> getReviews(){return _reviews;}
 
+  //Calculates and returns the review mean or average
   double getReviewAverage(){
+    //Keeps track of the total reviews in the list.
     var totalReviews = _reviews.length;
 
+    //if there are no reviews, then the average is 0.0.
     if (totalReviews == 0){
       return 0.0;
     }
 
+    //Initializes a totalScore variable to keep track of the total score. 
+    //Then it iterates through the list of reviews and adds all the ratings together.
     var totalScore = 0.0;
     for (var r in _reviews){
       totalScore += r.getRating();
     }
 
+    //Returns the total score divided by the total reviews.
     return totalScore / totalReviews;
   }
 
@@ -141,11 +142,15 @@ class Book{
   String getOwner(){return _owner;}
 }
 
+//User class to handle ownership and other user things
 class User{
+  //initializes the user's name
   var _user = "";
 
+  //constructor for User
   User(this._user);
 
+  //Normal setters and getters for User class
   void _setUser(String user){
     _user = user;
   }
@@ -155,20 +160,26 @@ class User{
   }
 }
 
+//Filter class to help with filtering books on the main page.
 class Filter{
+  //Initializes the author, title, genre, and user text to aid in filtering.
   String _authorText = "Author: ";
   String _titleText = "Title: ";
   String _genreText = "Genre: ";
   String _userText = "User: ";
 
+  //Constructor that resets the author, title, genre, and user text
   Filter(){
     _reset();
   }
 
+  //Normal getters for the author, title, genre, and user text.
   String _getAuthorText(){return _authorText;}
   String _getTitleText(){return _titleText;}
   String _getGenreText(){return _genreText;}
   String _getUserText(){return _userText;}
+
+  //This gets the only text variable that contains a value and returns it.
   String _text(){
     if (_authorText.isNotEmpty){
       return _authorText;
@@ -183,6 +194,9 @@ class Filter{
     }
   }
 
+  //These functions add text to the variables initialized above.
+  //It first resets the variables, then appends the text to the appropriate variable
+  //then finally it clears all of the variables but the one appended to.
   void _addAuthorText(String text){
     _reset();
     _authorText += text;
@@ -207,6 +221,8 @@ class Filter{
     _clearAllBut(_userText);
   }
 
+  //This clears all of the variables intialized at the top except
+  //for the one that starts the same as the text passed into this.
   void _clearAllBut(String text){
     if (!text.contains(_authorText)){
       _authorText = "";
@@ -225,6 +241,7 @@ class Filter{
     }
   }
 
+  //resets all of the variables to their initial values.
   void _reset(){
     _authorText = "Author: ";
     _titleText = "Title: ";
@@ -233,22 +250,29 @@ class Filter{
   }
 }
 
+//This is a map type structure to keep track of an index and a value corresponding to that index.
 class ValueMap{
+  //Initializes the index and value
   int _index = 0;
   String _value = "";
 
+  //Constructor class
   ValueMap(int index, String value){
     _index = index;
     _value = value;
   }
 
+  //Normal getters since these values should not be changed, so there is no need for setters.
   int _getIndex(){return _index;}
   String _getValue(){return _value;}
 }
 
+//The actual homepage stuff
 class _MyHomePageState extends State<MyHomePage> {
+  //This list of colors keeps track of the different color of books on the main page. Allows for alternating between these three books.
   List<Color> bookColors = [Color(0xFF9C6644), Color(0xFF7F5539), Color(0xFFB08968)];
 
+  //Some constant variables to ensure easy and proper sizing and colors.
   static const _filterBoxSize = 250.0;
   static const _boxSpacing = 40.0;
   static const _defaultPageColor = Color(0xFFEDE0D4);
@@ -257,20 +281,28 @@ class _MyHomePageState extends State<MyHomePage> {
   static const _subHeadingFontSize = 25.0;
   static const _bookFontSize = 20.0;
 
+  //Some text editing controllers that allow me to get the text entered from the filter menu.
   static final TextEditingController _authorTextController = TextEditingController();
   static final TextEditingController _titleTextController = TextEditingController();
   static final TextEditingController _genreTextController = TextEditingController();
 
-  List<Review> r = [Review("","", 0.0)];
-
+  //Some helper variables to determine certain filtering. 
+  //The personal library bool keeps track of whether only the user's books
+  //show, or if all books show.
+  //The show clear filter button determines whether the button to clear
+  //the filter is shown.
   bool _personalLibrary = false;
   bool _showClearFilterButton = false;
 
+  //Some more helper variables to keep track of which way to sort the books
+  //given a specific part of the book i.e. author, title, genre, review
   int authorSort = 0;
   int titleSort = 0;
   int genreSort = 0;
   int reviewSort = 0;
 
+  //generates a temporary list of books for testing
+  //TODO: change this to only include books gotten from an API call to the local database.
   late List<Book> books = [
     Book("Super long title that probably won't fit on the thingamabob", "", "", "", "", "", "Jo"),
     Book("Harry Potter and the Sorcerer's Stone", "Fantasy", "J. K. Rowling", "", "", "", "Jim"),
@@ -286,35 +318,55 @@ class _MyHomePageState extends State<MyHomePage> {
     Book("Title11", "", "i", "", "", "", "Bob"),
     Book("Title12", "", "j", "", "", "", "Bob")];
 
+  //Creates a user variable to test out the 
+  //TODO: change this to only include the user gotten from an API call to the local database.
   User user = User("Bob");
 
+  //This creates a list of filteredbooks for the use of filtering later on.
   late List<Book> filteredBooks = books;
 
+  //simple helper class to invert a color. Mainly used for text on the page.
   Color _invertColor(Color color){
+    //Gets the rgb and subtracts those from 255.
     final r = 255 - (color.r * 255).round();
     final g = 255 - (color.g * 255).round();
     final b = 255 - (color.g * 255).round();
 
+    //Converts the rgb to color and returns the color
     return Color.fromARGB((color.a * 255).round(), r, g, b);
   }
 
+  //searchs through the list of valuemaps given to find the target given.
   List<ValueMap> _search(List<ValueMap> values, String target){
+    //initializes the low and high values
+    //These are necessary for binary search.
     int low = 0;
     int high = values.length - 1;
 
+    //initializes the found values variable to hold the values that match the target.
     List<ValueMap> foundValues = [];
 
+    //iterates through until low is greater than high.
     while (low <= high){
+      //Splits the list in half to try and find the value.
       int mid = ((high + low) ~/ 2);
 
+      //initializes a boolean to see if the value and target match, and 
+      //initializes a comparison result to see if the target is on the left or right side of the split.
       bool contains = values[mid]._getValue().contains(target);
       int comparisonResult = values[mid]._getValue().compareTo(target);
 
+      //Checks if the comparison result is zero (or it matches) or checks if the value contains the target (which is also a match).
       if (comparisonResult == 0 || contains){
+        //Adds the value to the found values list.
         foundValues.add(values[mid]);
+
+        //Loops from one after the mid variable until the end of the list is met to find any more matches.
         for (int i = mid + 1; i < values.length; i++){
+          //checks it the value contains the target
           bool comparisonResult = values[i]._getValue().contains(target);
 
+          //If so, add the value to the found values target and continue on, otherwise stop iterating.
           if (comparisonResult){
             foundValues.add(values[i]);
           }else{
@@ -322,9 +374,13 @@ class _MyHomePageState extends State<MyHomePage> {
           }
         }
 
+        //Loops from one before the mid variable until reaching the start of the list.
         for (int i = mid - 1; i >= 0; i--){
+          //Checks if the value contains the target.
           bool comparisonResult = values[i]._getValue().contains(target);
 
+          //If it does contain the target, add the value to found values,
+          //otherwise, exit the loop.
           if (comparisonResult){
             foundValues.add(values[i]);
           }else{
@@ -332,16 +388,22 @@ class _MyHomePageState extends State<MyHomePage> {
           }
         }
 
+        //Return the values that were found.
         return foundValues;
       }
+      //if the compared result is negative, then we want to go to the right side of the list
       else if (comparisonResult < 0){
         low = mid + 1;
       }
+      //This is the last check, which means the comparison result must be larger than 0
+      //So we want to go to the left of the list. 
       else{
         high = mid - 1;
       }
+      //Iterates again, so it splits it in half until we find a match.
     }
 
+    //Returns foundValues (Which should be nothing since we got out of the while loop.
     return foundValues;
   }
 
